16fd6ec7d6a3e2a190536e0bfc0036bab6790160
LukasWestholt <support@lukaswestholt.de>
2025-05-04 12:27:30 +0200

    style: install-or-update.sh

--- modules/install-or-update.sh
+++ modules/install-or-update.sh
@@ -1,1 +1,20 @@
-(cd MMM-PublicTransportHafas && npm ci) && (cd MMM-Pir && npm install) && (cd MMM-Bring && npm install) && (cd mmm-systemtemperature && npm install) && (cd MMM-Hue-Controller-2 && npm ci) && (cd MMM-Cinestar-FDW && npm install) && (cd MMM-Screencast && npm install)
+#!/bin/bash
+set -e # abort any time an error occurred
+
+# List of modules and their install commands
+declare -A modules=(
+  [MMM-PublicTransportHafas]="npm ci"
+  [MMM-Pir]="npm install"
+  [MMM-Bring]="npm install"
+  [mmm-systemtemperature]="npm install"
+  [MMM-Hue-Controller-2]="npm ci"
+  [MMM-Cinestar-FDW]="npm install"
+  [MMM-Screencast]="npm install"
+)
+
+for dir in "${!modules[@]}"; do
+  echo "Installing dependencies in $dir..."
+  (cd "$dir" && ${modules[$dir]} --no-audit)
+done
+
+
